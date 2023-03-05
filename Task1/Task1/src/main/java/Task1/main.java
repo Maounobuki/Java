dcb4aeb3f67182b79ab884843fa0b388dfaeaa20
Maounobuki <116195152+Maounobuki@users.noreply.github.com>
2023-03-05 18:02:02 +0300

    Solved second objective at task1

--- Task1/Task1/src/main/java/Task1/main.java
+++ Task1/Task1/src/main/java/Task1/main.java
@@ -1,3 +1,16 @@
+//Урок 1. Знакомство с языком программирования Java
+//        1. Задать одномерный массив и найти в нем минимальный и максимальный элементы
+//        .
+//
+//        2. Написать метод, который определяет, является ли год високосным,
+//        и возвращает boolean (високосный - true, не високосный - false).
+//        Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+//        .
+//
+//        3. Дан массив nums = [3,2,2,3] и число val = 3.
+//        Если в массиве есть числа, равные заданному, нужно перенести эти элементы в конец массива.
+//        Таким образом, первые несколько (или все) элементов массива должны быть отличны от заданного, а остальные - равны ему.
+
 package Task1;
 import java.util.Scanner;
 import java.util.Arrays;
@@ -7,46 +20,71 @@ import java.util.Random;
 
 public class main {
     public static void main(String[] args){
-        int [] array = FillArray();
+       //Objective 1
+        /*int [] array = FillArray();
         System.out.println("Итоговый массив: "+Arrays.toString(array));
         PrintMinMax(array);
+
+         */
+        //Objective 2
+        IsLeapYear();
+
+
     }
 
-        private static int[] FillArray()
-    {
+//     Objective 1 methods
+//        private static int[] FillArray()
+//    {
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите количество элементов массива:");
+//        int limit  = in.nextInt();
+//        int [] numbers = new int[limit];
+//        System.out.print("Введите минимальное значение элементов массива:");
+//        int min_value = in.nextInt();
+//        System.out.print("Введите максимальное значение элементов массива:");
+//        int max_value = in.nextInt();
+//        if  (min_value > max_value) {
+//            System.out.println("Вы ввели неверные значения,  попробуйте ещё раз, следуя инструкциям!");
+//            FillArray();}
+//        else {
+//            for (int i = 0; i < numbers.length; i++) {
+//                numbers[i] = new Random().nextInt(min_value, max_value + 1);
+//            }
+//        }
+//       return numbers;
+//
+//    }
+//
+//    private static void PrintMinMax(int[] arr)
+//        {
+//
+//
+//        int max = Arrays.stream(arr)
+//                .max()
+//                .getAsInt();
+//
+//        int min = Arrays.stream(arr)
+//                .min()
+//                .getAsInt();
+//
+//        System.out.println("Минимальный элемент массива: " + min);
+//        System.out.println("Максимальный элемент массива: " + max);
+//    }
+//}
+
+//     Objective 2 methods
+    public static boolean IsLeapYear() {
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите количество элементов массива:");
-        int limit  = in.nextInt();
-        int [] numbers = new int[limit];
-        System.out.print("Введите минимальное значение элементов массива:");
-        int min_value = in.nextInt();
-        System.out.print("Введите максимальное значение элементов массива:");
-        int max_value = in.nextInt();
-        if  (min_value > max_value) {
-            System.out.println("Вы ввели неверные значения,  попробуйте ещё раз, следуя инструкциям!");
-            FillArray();}
-        else {
-            for (int i = 0; i < numbers.length; i++) {
-                numbers[i] = new Random().nextInt(min_value, max_value + 1);
-            }
+        System.out.print("Введите год:");
+        int year  = in.nextInt();
+
+        if (year % 4 != 0 || year % 100 == 0 && year % 400 != 0){
+            System.out.print("false");
+            return false;
         }
-       return numbers;
-
+        else {
+            System.out.print("true");
+            return true;
+        }
     }
-
-    private static void PrintMinMax(int[] arr)
-        {
-
-
-        int max = Arrays.stream(arr)
-                .max()
-                .getAsInt();
-
-        int min = Arrays.stream(arr)
-                .min()
-                .getAsInt();
-
-        System.out.println("Минимальный элемент массива: " + min);
-        System.out.println("Максимальный элемент массива: " + max);
     }
-}
