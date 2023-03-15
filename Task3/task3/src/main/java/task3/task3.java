663e76cb3cb0ee3e8b8fc9ecac0336ac07c06e46
Maounobuki <116195152+Maounobuki@users.noreply.github.com>
2023-03-15 17:27:41 +0300

    task3 solved first objective

--- Task3/task3/src/main/java/task3/task3.java
+++ Task3/task3/src/main/java/task3/task3.java
@@ -1,4 +1,41 @@
 package task3;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class task3 {
+    public static void main(String[] args) {
+        evenRemove(fillArray());
+
+
 }
+
+//    Пусть дан произвольный список целых чисел, удалить из него четные числа
+//   (в языке уже есть что-то готовое для этого)
+
+    public static ArrayList<Integer> fillArray (){
+        ArrayList<Integer> nums = new ArrayList<>();
+        Random random = new Random();
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Введите размер массива: ");
+        Integer size = in.nextInt();
+        System.out.printf("Введите верхний предел массива: ");
+        Integer maxValue = in.nextInt();
+        for (int i = 0; i < size; i++) {
+            nums.add(random.nextInt(maxValue));
+        }
+        System.out.println(nums.toString());
+        return nums;
+    }
+
+    private static void evenRemove (ArrayList<Integer> nums){
+
+        nums.removeIf(i -> i%2 == 0 && i!=0);
+        System.out.println(nums.toString());
+    }
+
+    }
+
+
+
