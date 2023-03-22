050c658e06239d6c84737c0a1dc9bc466a13c460
Maounobuki <116195152+Maounobuki@users.noreply.github.com>
2023-03-22 21:20:15 +0300

    task5 solved second objective

--- Task5/Task5/src/main/java/Task5/Task5.java
+++ Task5/Task5/src/main/java/Task5/Task5.java
@@ -20,6 +20,9 @@ public class Task5 {
             addNumber("Petrov", 787897, bookPhone);
             printBook(bookPhone);
 
+
+            repeatCount();
+
         }
 
 
@@ -46,7 +49,63 @@ public class Task5 {
                 System.out.printf("%s: %s \n", item.getKey(), phones);
             }
         }
+    // Задание 2. Пусть дан список сотрудников:
+    // Написать программу, которая найдёт и выведет повторяющиеся имена с количеством повторений.
+    // Отсортировать по убыванию популярности.
+    private static void printWorkersList(Map<String, ArrayList<String>> database) {
+        for (String name : database.keySet()) {
+            System.out.print(name + " :  ");
+            for (String telephon : database.get(name)) {
+                System.out.printf(telephon + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    private static void repeatCount() {
 
+        ArrayList<String> workersList = new ArrayList<>();
+        workersList.add("Иван Иванов");
+        workersList.add("Кристина Белова");
+        workersList.add("Светлана Петрова");
+        workersList.add("Анна Мусина");
+        workersList.add("Анна Крутова");
+        workersList.add("Иван Юрин");
+        workersList.add("Петр Лыков");
+        workersList.add("Павел Чернов");
+        workersList.add("Петр Чернышов");
+        workersList.add("Мария Федорова");
+        workersList.add("Марина Светлова");
+        workersList.add("Мария Савина");
+        workersList.add("Мария Рыкова");
+        workersList.add("Марина Лугова");
+        workersList.add("Анна Владимирова");
+        workersList.add("Иван Мечников");
+        workersList.add("Петр Петин");
+        workersList.add("Иван Ежов");
+
+        Map<String, ArrayList<String>> workersMap = new HashMap<>();
+
+        for (String string : workersList) {
+            String[] worker = string.split(" ");
+            if (workersMap.containsKey(worker[0])){
+                workersMap.get(worker[0]).add(worker[1]);
+            }
+            else{
+                workersMap.put(worker[0],new ArrayList<>(Arrays.asList(worker[1])));
+            }
+        }
+        printWorkersList(workersMap);
+        printNumberOfRepetitions(workersMap);
+    }
+
+    private static void printNumberOfRepetitions(Map<String, ArrayList<String>> workersMap) {
+        for (String key : workersMap.keySet()) {
+            if (workersMap.get(key).size()>1){
+                System.out.println(key + " " + workersMap.get(key).size() );
+            }
+        }
+    }
 
     }
 
